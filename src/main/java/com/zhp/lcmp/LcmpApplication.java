80bbd4b236c7122ee5f1c32b06de666b3173fb61
Zhao-HP <loner_0215@163.com>
2019-12-15 14:38:31 +0800

    初始化

---
+++ src/main/java/com/zhp/lcmp/LcmpApplication.java
@@ -0,0 +1,13 @@
+package com.zhp.lcmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhp/lcmp/LcmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhp.lcmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
